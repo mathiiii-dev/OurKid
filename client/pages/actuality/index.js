90c8d3bbd4079d8996bb205a60927313b7ed4fc8
Mathias <mat.micheli99@gmail.com>
2021-11-06 18:56:23 +0100

    feat: upgrade of actuality form

--- client/pages/actuality/index.js
+++ client/pages/actuality/index.js
@@ -1,21 +1,18 @@
-import {SimpleGrid} from '@mantine/core';
+import {SimpleGrid, Textarea, TextInput, Input, InputWrapper, Group} from '@mantine/core';
 import {Button, Paper, Text} from "@mantine/core";
 import {useState} from "react";
-import dynamic from 'next/dynamic';
+import {Dropzone} from "@mantine/dropzone";
 
-const RichTextEditor = dynamic(() => import('@mantine/rte'), {
-    ssr: false,
-    loading: () => null
-});
 
 function Actuality({posts}) {
-    const [post, setPost] = useState();
+
     const addActuality = async event => {
         await fetch(
             'http://127.0.0.1:8000/actuality',
             {
                 body: JSON.stringify({
-                    post
+                    title: event.target.title.value,
+                    description: event.target.description.value,
                 }),
                 headers: {
                     'Content-Type': 'application/json'
@@ -25,25 +22,65 @@ function Actuality({posts}) {
         )
     }
 
-    const handleImageUpload = (file) =>
-    new Promise((resolve, reject) => {
-        const formData = new FormData();
-        formData.append('image', file);
+    const handleImageUpload = (file) => new Promise((resolve, reject) => {
+            fetch('https://content.dropboxapi.com/2/files/upload', {
+                headers: {
+                    'Dropbox-API-Arg': JSON.stringify({
+                        "path": "/test.png",
+                        "mode": "add",
+                        "autorename": true,
+                        "mute": false,
+                        "strict_conflict": false
+                    }),
+                    'Content-Type': 'application/octet-stream',
+                    'Authorization': `Bearer ${process.env.DROPBOX_API_KEY}`
+                },
+                method: 'POST',
+                body: file,
+            })
+                .then((response) => response.json())
+                .then((result) => console.log(result))
+                .catch(() => reject(new Error('Upload failed')));
+        }
+    );
 
-        console.log(formData)
-    });
+    const [photo, setPhoto] = useState(0);
 
     return (
         <>
             <form onSubmit={addActuality}>
-                <RichTextEditor sticky={false} value={post} onChange={setPost}/>
+                <TextInput label="Titre" required id={"title"}/>
+                <Textarea label="Description" id={"description"} required/>
+                <InputWrapper id="photo" label="Photo">
+                    {[...Array(photo)].map((photo, index) => {
+                        return <Dropzone key={index} onDrop={console.log} maxSize={3 * 720 ** 2}>
+                            {(status) => (
+                                <Group position="center" spacing="xl" style={{minHeight: 220, pointerEvents: 'none'}}>
+                                    <div>
+                                        <Text size="xl" inline>
+                                            Drag images here or click to select files
+                                        </Text>
+                                        <Text size="sm" color="dimmed" inline mt={7}>
+                                            Attach as many files as you like, each file should not exceed 5mb
+                                        </Text>
+                                    </div>
+                                </Group>
+                            )}
+                        </Dropzone>
+                    })}
+                </InputWrapper>
+                <Button onClick={() => setPhoto(photo + 1)}>Ajouter une image</Button>
+                {photo !== 0 ?
+                    <Button onClick={() => setPhoto(photo - 1)}>Supprimer une image</Button>
+                    : ''
+                }
                 <Button type="submit">Envoyer</Button>
             </form>
             <SimpleGrid cols={1}>
                 {posts.map(res =>
                     <Paper key={res.id} padding="md" shadow="md" withBorder>
-                        <h2>Nouvelle actualité du {new Date(res.createdAt).getDay() + '/' + new Date(res.createdAt).getMonth()}</h2>
-                        <Text dangerouslySetInnerHTML={{__html: res.post}}/>
+                        <h2>{res.title + ' ' + new Date(res.createdAt).getDay() + '/' + new Date(res.createdAt).getMonth()}</h2>
+                        <Text dangerouslySetInnerHTML={{__html: res.description}}/>
                     </Paper>
                 )}
             </SimpleGrid>
