9f83bff5168a8036041d735293a8266bd9513ac3
Mathias <mat.micheli99@gmail.com>
2021-11-11 21:24:20 +0100

    feat: secured parent and kid form

--- client/pages/registration/parent.js
+++ client/pages/registration/parent.js
@@ -1,15 +1,30 @@
 import {Input, InputWrapper, Button, PasswordInput} from "@mantine/core";
 import {useNotifications} from '@mantine/notifications';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTimes, faCheck} from '@fortawesome/free-solid-svg-icons'
+import {faCheck} from '@fortawesome/free-solid-svg-icons'
+import {useForm} from "@mantine/hooks";
 
 function Parent() {
     const notifications = useNotifications();
+    const form = useForm({
+        initialValues: {
+            firstname: '',
+            lastname: '',
+            password: '',
+            email: ''
+        },
 
-    const registerUser = async event => {
+        validationRules: {
+            firstname: (firstname) => firstname.trim().length >= 2,
+            lastname: (lastname) => lastname.trim().length >= 2,
+            password: (password) => password.trim().length >= 2,
+            email: (email) => email.trim().length >= 2
+        },
+    });
+    const registerUser = event => {
         event.preventDefault()
-
-        const res = await fetch(
+        form.validate()
+        fetch(
             'http://127.0.0.1:8000/registration/parent',
             {
                 body: JSON.stringify({
@@ -23,25 +38,17 @@ function Parent() {
                 },
                 method: 'POST'
             }
-        )
-
-        const result = await res.json()
-
-        if (res.status !== 200) {
-            notifications.showNotification({
-                title: "Erreur lors de l'envoie du formaulaire",
-                message: result.error_description,
-                color: "red",
-                icon: <FontAwesomeIcon icon={faTimes}/>
-            })
-        } else {
-            notifications.showNotification({
-                title: 'Bravo !',
-                message: 'Vous avez été enregistré avec succes',
-                color: "green",
-                icon: <FontAwesomeIcon icon={faCheck}/>
-            })
-        }
+        ).then(r => {
+            if (r.status === 200) {
+                form.reset()
+                notifications.showNotification({
+                    title: 'Bravo !',
+                    message: 'Vous avez été enregistré avec succes',
+                    color: "green",
+                    icon: <FontAwesomeIcon icon={faCheck}/>
+                })
+            }
+        })
     }
 
     return (
@@ -51,28 +58,41 @@ function Parent() {
                     id="firstname"
                     required
                     label="Prénom"
+                    error={form.errors.firstname && 'Veuillez saisir un prénom'}
                 >
-                    <Input id="firstname" placeholder="Adele"/>
+                    <Input id="firstname" placeholder="Adele"
+                           value={form.values.firstname}
+                           onChange={(event) => form.setFieldValue('firstname', event.currentTarget.value)}/>
                 </InputWrapper>
                 <InputWrapper
                     id="lastname"
                     required
                     label="Nom"
+                    error={form.errors.firstname && 'Veuillez saisir un nom'}
                 >
-                    <Input id="lastname" placeholder="Laurie"/>
+                    <Input id="lastname" placeholder="Laurie"
+                           value={form.values.lastname}
+                           onChange={(event) => form.setFieldValue('lastname', event.currentTarget.value)}/>
                 </InputWrapper>
                 <PasswordInput
                     id="password"
                     placeholder="********"
                     label="Mot de passe"
                     required
+                    error={form.errors.password && 'Veuillez saisir un titre'}
+                    value={form.values.password}
+                    onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
                 />
                 <InputWrapper
                     label="Email"
-                    required>
+                    required
+                    error={form.errors.email && 'Veuillez saisir un email'}
+                >
                     <Input
                         id="email"
                         placeholder="my-email@mail.com"
+                        value={form.values.email}
+                        onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
                     />
                 </InputWrapper>
                 <Button type="submit">Inscription</Button>
