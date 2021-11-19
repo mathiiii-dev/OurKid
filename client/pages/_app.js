ba6c6ef9c0f4f9acf7d2114f37b4ac1ad725c5ad
Mathias <mat.micheli99@gmail.com>
2021-11-19 23:20:30 +0100

    feat: first commit of homepage

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,39 @@
 import '../styles/globals.css'
 import {NotificationsProvider} from '@mantine/notifications';
-import {Container} from '@mantine/core';
+import {AppShell, Burger, Container, Header, MediaQuery, Text} from '@mantine/core';
+import {useState} from "react";
 
 function App({Component, pageProps}) {
+    const [opened, setOpened] = useState(false);
     return (
-        <Container>
-            <NotificationsProvider>
-                <Component {...pageProps}/>
-            </NotificationsProvider>
-        </Container>
+        <AppShell
+            // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
+            navbarOffsetBreakpoint="sm"
+            // fixed prop on AppShell will be automatically added to Header and Navbar
+            fixed
+            header={
+                <Header height={70} padding="md">
+                    {/* You can handle other responsive styles with MediaQuery component or createStyles function */}
+                    <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+                            <Burger
+                                opened={opened}
+                                onClick={() => setOpened((o) => !o)}
+                                size="sm"
+                                mr="xl"
+                            />
+                        </MediaQuery>
+                        <Text>Application header</Text>
+                    </div>
+                </Header>
+            }
+        >
+            <Container>
+                <NotificationsProvider>
+                    <Component {...pageProps}/>
+                </NotificationsProvider>
+            </Container>
+        </AppShell>
     );
 }
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,27 @@
+import {Center, SimpleGrid, Space, Title, Button} from "@mantine/core";
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <>
+            <Center>
+                <Title order={1}>OurKid</Title>
+            </Center>
+            <Space h="xl"/>
+            <Center>
+                <Title order={2}>Le site de gestion parfait pour vos enfants</Title>
+            </Center>
+            <Space h="xl"/>
+            <Center>
+                <SimpleGrid cols={2}>
+                    <div>
+                        <Button>Nourrice</Button>
+                    </div>
+                    <div>
+                        <Button>Parent</Button>
+                    </div>
+                </SimpleGrid>
+            </Center>
+        </>
     )
 }
 
